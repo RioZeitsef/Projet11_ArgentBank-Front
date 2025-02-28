c048aaf83688b0c34b615aeecccbbbe168fb7d00
RioZeitsef <metz.matthieu@gmail.com>
2025-02-28 18:13:46 +0100

    Create the Header and the routing neccessary for the signUp Components (coming soon)

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Header from "./components/Header";
+import Survey from "./Pages/Survey";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signIn" element={<Survey />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../css/Header.module.css";
+import argentBankLogo from "../assets/argentBankLogo.png";
 
-const Header = () => {
+function Header() {
+  return (
+    <nav className={styles.mainnav}>
+      <Link to="/" className={styles.mainnavlogo}>
+        <img
+          className={styles.mainnavlogoimage}
+          src={argentBankLogo}
+          alt="Argent Bank Logo"
+        />
+      </Link>
+      <div>
+        <Link to="/Survey" className={styles.mainnavitem}>
+          <i class="fa fa-user-circle"></i>
+          Sign In
+        </Link>
+      </div>
+    </nav>
+  );
+}
 
-};
+export default Header;
