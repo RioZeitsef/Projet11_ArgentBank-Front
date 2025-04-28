770e3dda1f387d00371a42a58c94df84c87384c2
RioZeitsef <metz.matthieu@gmail.com>
2025-04-28 23:51:06 +0200

    Delete the token in the localstorage and use the redux-persist library to fix the automatic persistance

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
+import type { AppDispatch } from "./store";
 import store from "./store";
+import { checkAuthentication } from "./slice/authSlice";
 import Home from "./Pages/Home";
 import Signin from "./Pages/Signin";
 import Layout from "./components/Layout";
 import User from "./Pages/User";
 
+// Composant qui vérifie l'authentification
+const AuthCheck: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const dispatch: AppDispatch = useDispatch();
+  
+  useEffect(() => {
+    // Vérifier l'authentification au chargement de l'application
+    dispatch(checkAuthentication());
+  }, [dispatch]);
+  
+  return <>{children}</>;
+};
+
 function App() {
   return (
     <Provider store={store}>
       <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/signin" element={<Signin />} />
-            <Route path="/user" element={<User />} />
-          </Route>
-        </Routes>
+        <AuthCheck>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/user" element={<User />} />
+            </Route>
+          </Routes>
+        </AuthCheck>
       </Router>
     </Provider>
   );
